ee300475a7499c23f90de51667f4a0107933a80d
Tiana-K08 <tetkononenko2020@gmail.com>
2025-05-29 20:17:16 +0200

    Select a random comment from a predefined list

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
 // Determine meal type based on user-provided time input
-let timeInput;
-const timePattern = /^(?:[0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
-let clientExit = '¡Vuelve pronto!\nSiempre eres bienvenido en Bottega Diner.';
-let mealType = '';
+// let timeInput;
+// const timePattern = /^(?:[0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+// let clientExit = '¡Vuelve pronto!\nSiempre eres bienvenido en Bottega Diner.';
+// let mealType = '';
 
-outerLoop: while (mealType == '' || mealType == 'invalid') {
-    timeInput = prompt('¡Bienvenido a Bottega Diner!\nNuestra cocina trabaja para usted todos los días de 08:30 a 23:00.\n\nPor favor, introduzca la hora actual en formato HH:MM');
-    if (timeInput === null) {
-        alert(clientExit);
-        break outerLoop;
-    } else {
-        timeInput = timeInput.trim();
-        if (timePattern.test(timeInput)) {
-            const [hours, minutes] = timeInput.split(':').map(Number);
-            if (hours < 8 || (hours == 8 && minutes < 30) || hours >= 23) {
-                mealType = 'closed';
-            } else if (hours < 12) {
-                mealType = 'breakfast';
-            } else if (hours < 18) {
-                mealType = 'lunch';
-            } else {
-                mealType = 'dinner';
-            }
-        } else {
-            mealType = 'invalid';
-            alert('Ha introducido un formato de hora incorrecto. Por favor, introduzca la hora en el formato HH:MM.');
-        }
-    }
+// outerLoop: while (mealType == '' || mealType == 'invalid') {
+//     timeInput = prompt('¡Bienvenido a Bottega Diner!\nNuestra cocina trabaja para usted todos los días de 08:30 a 23:00.\n\nPor favor, introduzca la hora actual en formato HH:MM');
+//     if (timeInput === null) {
+//         alert(clientExit);
+//         break outerLoop;
+//     } else {
+//         timeInput = timeInput.trim();
+//         if (timePattern.test(timeInput)) {
+//             const [hours, minutes] = timeInput.split(':').map(Number);
+//             if (hours < 8 || (hours == 8 && minutes < 30) || hours >= 23) {
+//                 mealType = 'closed';
+//             } else if (hours < 12) {
+//                 mealType = 'breakfast';
+//             } else if (hours < 18) {
+//                 mealType = 'lunch';
+//             } else {
+//                 mealType = 'dinner';
+//             }
+//         } else {
+//             mealType = 'invalid';
+//             alert('Ha introducido un formato de hora incorrecto. Por favor, introduzca la hora en el formato HH:MM.');
+//         }
+//     }
+// }
+
+// Select a random comment from a predefined list
+function randomComment() {
+    const comments = [
+        'Recomendación del chef. ¡Simplemente espectacular!',
+        '¡Este plato alegra cualquier día!',
+        'Uno de los favoritos de nuestros clientes.',
+        '¡Excelente elección! Este plato es muy popular.',
+        '¡Elección perfecta! Estás a la moda culinaria.',
+        '¡Una delicia! No te arrepentirás.'
+    ];
+    return comments[Math.floor(Math.random() * comments.length)];
 }
 
 
-
-console.log(mealType);
+console.log(randomComment());
